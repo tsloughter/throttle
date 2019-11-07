f1ec4777c59b5051dbb9c4e3c7f032118c355c45
Tristan Sloughter <t@crashfast.com>
2019-11-07 11:45:38 -0700

    use record for state and boolean for persistent_term value

--- src/throttle.erl
+++ src/throttle.erl
@@ -3,57 +3,58 @@
 -export([init/3,
          reconfigure/3,
          self_check/1,
-         is_throttled/1,
-         throttle/1,
+         is_enabled/1,
+         disable/1,
          erase/1]).
 
 -define(KEY(Name), {?MODULE, Name}).
 
--spec init(term(), integer(), integer()) -> {ok, atomics:atomics_ref()}.
-init(Name, Hwm, Window) when Hwm > 0, Window > 0 ->
-    %% keeping the hwm in an atomic lets us reconfigure
-    %% without changing the persistent term
-    A = atomics:new(3, []),
-    atomics:put(A, 1, 0),
-    atomics:put(A, 2, Hwm),
-    atomics:put(A, 3, Hwm - Window),
-    persistent_term:put(?KEY(Name), A),
-    {ok, A}.
+-record(state, {name            :: term(),
+                high_water_mark :: integer(),
+                window_min      :: integer()}).
+-opaque state() :: #state{}.
+-export_type([state/0]).
 
--spec reconfigure(term(), integer(), integer()) -> ok.
-reconfigure(Name, Hwm, Window) when Hwm > 0, Window > 0 ->
-    A = persistent_term:get(?KEY(Name)),
-    atomics:put(A, 2, Hwm),
-    atomics:put(A, 3, Hwm - Window),
-    ok.
+-spec init(term(), integer(), integer()) -> {ok, state()}.
+init(Name, Hwm, Window) when Hwm > 0, Window > 0 ->
+    persistent_term:put(?KEY(Name), true),
+    {ok, #state{name=Name,
+                high_water_mark=Hwm,
+                window_min=Hwm - Window}}.
+
+-spec reconfigure(integer(), integer(), state()) -> state().
+reconfigure(Hwm, Window, State) when Hwm > 0, Window > 0 ->
+    State#state{high_water_mark=Hwm,
+                window_min=Hwm - Window}.
 
 %% Does the message queue check and returns the new value as a boolean
 -spec self_check(term()) -> boolean().
-self_check(Name) ->
-    A = persistent_term:get(?KEY(Name)),
-    Current = atomics:get(A, 1),
-    Hwm = atomics:get(A, 2),
-    WindowMin = atomics:get(A, 3),
-    case {Current, erlang:process_info(self(), message_queue_len)} of
-        {0, {message_queue_len, Len}} when Len > Hwm ->
-            atomics:exchange(A, 1, 1),
+self_check(#state{name=Name,
+                  high_water_mark=Hwm,
+                  window_min=WindowMin}) ->
+    IsEnabled = is_enabled(Name),
+    case {IsEnabled, erlang:process_info(self(), message_queue_len)} of
+        {true, {message_queue_len, Len}} when Len > Hwm ->
+            ok = disable(Name),
             true;
-        {1, {message_queue_len, Len}} when Len < WindowMin ->
-            atomics:exchange(A, 1, 0),
+        {false, {message_queue_len, Len}} when Len < WindowMin ->
+            ok = enable(Name),
             false;
-        {Current, _} ->
-            Current
+        {IsEnabled, _} ->
+            IsEnabled
     end.
 
--spec is_throttled(term()) -> boolean().
-is_throttled(Name) ->
-    A = persistent_term:get(?KEY(Name)),
-    atomics:get(A, 1) =:= 1.
+-spec is_enabled(term()) -> boolean().
+is_enabled(Name) ->
+    persistent_term:get(?KEY(Name)).
 
--spec throttle(term()) -> integer().
-throttle(Name) ->
-    A = persistent_term:get(?KEY(Name)),
-    atomics:exchange(A, 1, 1).
+-spec enable(term()) -> ok.
+enable(Name) ->
+    persistent_term:put(?KEY(Name), true).
+
+-spec disable(term()) -> ok.
+disable(Name) ->
+    persistent_term:put(?KEY(Name), false).
 
 -spec erase(term()) -> boolean().
 erase(Name) ->
